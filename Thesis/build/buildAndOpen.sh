af1ed8370dd626280bbf3c945fd11ecf20d4f557
Mout Pessemier <moutpessemier@hotmail.com>
2022-05-23 22:33:36 +0200

    Update buildAndOpen.sh

--- Thesis/build/buildAndOpen.sh
+++ Thesis/build/buildAndOpen.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
 
+#Build to create all generated files
 pdflatex -synctex=1 -interaction=nonstopmode "Masterproef".tex
+
+#Make reference list
 bibtex "Masterproef".aux
+
+#Make glossaries
 makeglossaries Masterproef
+
+#Build again after generating everyting
 pdflatex -synctex=1 -interaction=nonstopmode "Masterproef".tex
+
+#Open the file
 open Masterproef.pdf
